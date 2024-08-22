8601af767df18c099cc16d37a113b9b2c584f8a3
AbdulMoeezImran <moeezimran6@gmail.com>
2024-08-22 17:21:22 +0500

    Fixed bugs.

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,42 +1,34 @@
 // import Image from "next/image";
 import Post from "../components/post";
 
-const post =  [{
-          id: 1,          
-          likes: 23,
-          description: "Bats are blood suckers.",
-          image: {
-              url: "/uploads/igam_ogam_VV_13d2ozcy4_unsplash_47af6e77c9.jpg"
-          }
-        },
-        {
-          id: 2,          
-          likes: 12,
-          description: "The worst thing about mentalilness is people expect you to behave as if you dont want.",
-          image: {
-              url: "/uploads/igam_ogam_VV_13d2ozcy4_unsplash_47af6e77c9.jpg"
-          }
-        },
-        {
-          id: 3,          
-          likes: 11,
-          description: "Bats are good blood suckers.",
-          image: {
-              url: "/uploads/igam_ogam_VV_13d2ozcy4_unsplash_47af6e77c9.jpg"
-          }
-        }
+const post = [
+  {
+    id: 1,
+    likes: 23,
+    description: "Bats are blood suckers.",
+    image: "/uploads/igam_ogam_VV_13d2ozcy4_unsplash_47af6e77c9.jpg",
+  },
+  {
+    id: 2,
+    likes: 12,
+    description:
+      "The worst thing about mentalilness is people expect you to behave as if you dont want.",
+    image: "/uploads/igam_ogam_VV_13d2ozcy4_unsplash_47af6e77c9.jpg",
+  },
+  {
+    id: 3,
+    likes: 11,
+    description: "Bats are good blood suckers.",
+    image: "/uploads/igam_ogam_VV_13d2ozcy4_unsplash_47af6e77c9.jpg",
+  },
 ];
 
-export default function Home({likes, description, image, id, image.url}) {  
-    return (
-      <>
-      {post.map((post) => (
-        <Post
-          key={id} 
-          likes={likes}
-          description={description}
-          url={image && image.url} />
-        ))}
-      </>
+export default function Home() {
+  return (
+    <>
+      {post.map(({ likes, description, image, id }) => (
+        <Post key={id} likes={likes} description={description} url={image} />
+      ))}
+    </>
   );
 }
--- frontend/components/post/index.jsx
+++ frontend/components/post/index.jsx
@@ -1,41 +1,12 @@
 import React from "react";
 
-const post = [{
-    id: 1,          
-    likes: 23,
-    description: "Bats are blood suckers.",
-    image: {
-        url: "/uploads/igam_ogam_VV_13d2ozcy4_unsplash_47af6e77c9.jpg"
-    }
-  },
-  {
-    id: 2,          
-    likes: 12,
-    description: "The worst thing about mentalilness is people expect you to behave as if you dont want.",
-    image: {
-        url: "/uploads/igam_ogam_VV_13d2ozcy4_unsplash_47af6e77c9.jpg"
-    }
-  },
-  {
-    id: 3,          
-    likes: 11,
-    description: "Bats are good blood suckers.",
-    image: {
-        url: "/uploads/igam_ogam_VV_13d2ozcy4_unsplash_47af6e77c9.jpg"
-    }
-  }
-];
-
-    const API_URL = 'http://localhost:1337';
-    const formatImageURL = (url) => `${API_URL}${url}`;
-    
-    export default ({posts}) => {
-        // console.log(post);
-        return (
-        <div className="px-0.75 py-1.25  rounded border border-[#a3a3a3]">
-            <img className="max-w-15.625" src={formatImageURL(url)} />
-            <h1>Likes: {likes}</h1>
-            <h1>Description: {description}</h1>
-        </div>
-        )
-    }
+export default ({ likes, description, url }) => {
+  // console.log(post);
+  return (
+    <div className="px-0.75 py-1.25  rounded border border-[#a3a3a3]">
+      <img className="max-w-15.625" src={`http://localhost:1337${url}`} />
+      <h1>Likes: {likes}</h1>
+      <h1>Description: {description}</h1>
+    </div>
+  );
+};
